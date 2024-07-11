ef2b23d3820710f7f64605b290362fc20e324b92
dan198gg <danzelgoncharenko@gmail.com>
2024-07-11 10:56:24 +0300

    AddedFlowWithWrongs

--- app/src/main/java/com/example/catsanddogs/MainActivity.kt
+++ app/src/main/java/com/example/catsanddogs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.catsanddogs
 import android.media.Image
 import android.os.Bundle
 import android.text.Layout.Alignment
+import android.util.Log
 import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -18,6 +20,13 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -26,37 +35,71 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.catsanddogs.ui.theme.CatOrDog
 import com.example.catsanddogs.ui.theme.CatsAndDogsTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var dog= mutableListOf(CatOrDog(type = "Акита-ину", img = R.drawable.akitainy),
-                CatOrDog(type = "Сибирский хаски", img = R.drawable.haski)
-            )
-            cardScroll(list = dog)
-        }
-    }
+            cardScroll()
+
+
+
+
 }
+}
+        }
+
 
 
 @Composable
-fun cardScroll(list: MutableList<CatOrDog>){
+fun cardScroll(){
 
+    var flow1 = getCatsOrDogs()
+    var corScope = rememberCoroutineScope()
+    var dog1 by rememberSaveable {
+        mutableStateOf<MutableList<CatOrDog>>(mutableListOf())
+    }
+    var i=0
+    LaunchedEffect(key1 = dog1) {
+        corScope.launch {
+            flow1.collect {
 
+                dog1.add(it)
+                Log.i("CAT",it.type.toString())
+                Log.i("CAT2",dog1[i].type.toString())
+                i+=1
+            }
+        }
+    }
 
-    Column(
+    LazyColumn(
         Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())) {
-        repeat(list.size) {
+) {
+        items(dog1.size){
             Card(Modifier.padding(30.dp)) {
-
-                androidx.compose.foundation.Image(painter = painterResource(id = list[it].img), contentDescription = "0", modifier = Modifier.padding(30.dp))
-                Text(text =list[it].type ,Modifier.align(androidx.compose.ui.Alignment.CenterHorizontally), fontSize = 25.sp)
+                Log.i("CARD",dog1.size.toString())
+                androidx.compose.foundation.Image(painter = painterResource(id = dog1[it].img), contentDescription = "0", modifier = Modifier.padding(30.dp))
+                Text(text =dog1[it].type ,Modifier.align(androidx.compose.ui.Alignment.CenterHorizontally), fontSize = 25.sp)
             }
         }
     }
 }
 
+
+
+fun getCatsOrDogs() : Flow<CatOrDog> = flow {
+    var dog= mutableListOf(CatOrDog(type = "Акита-ину", img = R.drawable.akitainy),
+        CatOrDog(type = "Сибирский хаски", img = R.drawable.haski),
+        CatOrDog(type = "Японский бобтейл", img = R.drawable.japanesecat)
+    )
+    repeat(dog.size){
+        emit(dog[it])
+    }
+}
