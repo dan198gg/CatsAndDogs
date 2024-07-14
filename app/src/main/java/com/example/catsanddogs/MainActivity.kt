154f62a6e2fb5c2d2e0acb6856ff2acc33ad8243
dan198gg <danzelgoncharenko@gmail.com>
2024-07-14 17:29:10 +0300

    RemakedToSharedFlowAndAddedTheAnimationBeta

--- app/src/main/java/com/example/catsanddogs/MainActivity.kt
+++ app/src/main/java/com/example/catsanddogs/MainActivity.kt
@@ -8,6 +8,11 @@ import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,12 +28,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,8 +45,11 @@ import com.example.catsanddogs.ui.theme.CatOrDog
 import com.example.catsanddogs.ui.theme.CatsAndDogsTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
@@ -47,42 +57,79 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val mutableCat= MutableStateFlow<MutableList<CatOrDog>>(
-            mutableListOf(CatOrDog(type = "Сибирский хаски", img = R.drawable.haski),
-            CatOrDog(type = "Японский бобтейл", img = R.drawable.japanesecat)))
-        val stateCat=mutableCat.asStateFlow()
         super.onCreate(savedInstanceState)
+
+
+//
+//        mutableListOf(CatOrDog(type = "Сибирский хаски", img = R.drawable.haski),
+//            CatOrDog(type = "Японский бобтейл", img = R.drawable.japanesecat))
         enableEdgeToEdge()
         setContent {
-            cardScroll(stateCat,mutableCat)
-
-
-
-
-}
-}
+            val mutableCat= MutableSharedFlow<Int>()
+            val stateCat=mutableCat.asSharedFlow()
+            var list= mutableListOf(CatOrDog(R.drawable.haski,"Хаски"), CatOrDog(R.drawable.akitainy,"акита-ину"),
+                CatOrDog(R.drawable.japanesecat,"Японский кот")
+            )
+            val corScope= rememberCoroutineScope()
+            LaunchedEffect(key1 = "") {
+                corScope.launch {
+                    repeat(list.size){
+                        mutableCat.emit(it)
+                    }
+                }
+            }
+            cardScroll(stateCat,mutableCat,list)
+            }
         }
+    }
 
 
 
 @Composable
-fun cardScroll(flow:StateFlow<MutableList<CatOrDog>>,mutableStateFlow: MutableStateFlow<MutableList<CatOrDog>>){
-    var cat=flow.collectAsState()
+fun cardScroll(flow:SharedFlow<Int>,mutableStateFlow: MutableSharedFlow<Int>,list: MutableList<CatOrDog>) {
+    val catNum by flow.collectAsState(initial = 0)
+    var cat = remember {
+        mutableStateListOf<CatOrDog>()
+    }
+    val boxVisible = remember { mutableStateOf(true) }
+
     var corScope = rememberCoroutineScope()
     LaunchedEffect(key1 = "") {
         corScope.launch {
-            flow.collect()
+            flow.collect {
+                cat.add(list[it])
+                delay(1000)
+            }
+
         }
     }
 
     LazyColumn(
         Modifier
             .fillMaxSize()
-) {
-        items(flow.value.size){
-            Card(Modifier.padding(30.dp)) {
-                androidx.compose.foundation.Image(painter = painterResource(id = cat.value[it].img), contentDescription = "0", modifier = Modifier.padding(30.dp))
-                Text(text =cat.value[it].type ,Modifier.align(androidx.compose.ui.Alignment.CenterHorizontally), fontSize = 25.sp)
+    ) {
+        items(catNum) {
+            AnimatedVisibility(
+                visible = boxVisible.value,
+                Modifier.padding(40.dp),
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+
+
+                Card(Modifier.padding(30.dp)) {
+                    androidx.compose.foundation.Image(
+                        painter = painterResource(id = cat[it].img),
+                        contentDescription = "0",
+                        modifier = Modifier.padding(30.dp)
+                    )
+                    Text(
+                        text = cat[it].type,
+                        Modifier.align(androidx.compose.ui.Alignment.CenterHorizontally),
+                        fontSize = 25.sp
+                    )
+                }
+
             }
         }
     }
